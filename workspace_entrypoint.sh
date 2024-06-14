de64b73fd80df63ba0635d6a76b4a6739d89a58f
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-06-15 01:45:30 +1000

    Update workspace_entrypoint.sh

--- workspace_entrypoint.sh
+++ workspace_entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# reload udev
+service udev reload
+service udev restart
+    
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "$KOBUKI_ROOT/install/setup.bash"
